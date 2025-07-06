86fb4e61a01d9fa97831e7e3cdcb7678cde58a7c
joejohn98 <joejohn048@gmail.com>
2025-07-06 23:23:31 +0530

    feat(ProductList): Fetch products from API on mount
    Add a `useEffect` hook to fetch a list of products from the FakeStoreAPI when the component initially renders.

--- src/ProductList.tsx
+++ src/ProductList.tsx
@@ -1,16 +1,33 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import type { Product } from "./types";
 
 const ProductList = () => {
-    const [products, setProducts] = useState<Product[]>([]);
-    const [searchTerm, setSearchTerm] = useState<string>("");
-    const [loading, setLoading] = useState<boolean>(false);
-    
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  const [products, setProducts] = useState<Product[]>([]);
+  const [searchTerm, setSearchTerm] = useState<string>("");
+  const [loading, setLoading] = useState<boolean>(false);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      setLoading(true);
+      try {
+        const response = await fetch(
+          "https://fakestoreapi.in/api/products?limit=20"
+        );
+        if (response.status === 200) {
+          const data = await response.json();
+          setProducts(data.products);
+        }
+      } catch (error) {
+        console.error("Error fetching products:", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchProducts();
+  }, []);
+
+  return <div></div>;
+};
 
 export default ProductList;
